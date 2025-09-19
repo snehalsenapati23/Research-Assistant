2fb4eb9798a7763f9df17cad0884bfa46c9ecdc8
snehalsenapati23 <omsenapati23@gmail.com>
2025-09-20 00:38:50 +0530

    Response is created

--- src/main/java/com/research/assistant/ResearchController.java
+++ src/main/java/com/research/assistant/ResearchController.java
@@ -2,9 +2,11 @@ package com.research.assistant;
 
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/research")
@@ -14,6 +16,8 @@ public class ResearchController {
 
     @PostMapping("/process")
     public ResponseEntity<String> processContent(@RequestBody ResearchRequest researchRequest) {
+
+        log.info("Processing research request {}", researchRequest);
         String result=researchService.processRequest(researchRequest);
 
         return ResponseEntity.ok(result);
